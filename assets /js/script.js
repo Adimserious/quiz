15930c19706cffed204a879d1f6ea517e60335d6
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-05-09 19:25:59 +0000

    Add media query for medium screen

---
+++ assets /js/script.js
@@ -0,0 +1,90 @@
+const licenceQuestions = [
+    {
+        question: 'Your passenger wants to discuss something with you during the journey. what should you do?',
+        answer: [
+            { text: "Concentrate on the discussion", correct: false },
+            { text: "Concentrate on the driving", correct: true },
+            { text: "Concentrate on both", correct: false },
+        ]
+    },
+    {
+        question: 'What could cause the vehicle to leave the road?',
+        answer: [
+            { text: "Tiredness", correct: false },
+            { text: "Distraction", correct: false },
+            { text: "Inattention", correct: true },
+        ]
+    },
+    {
+        question: 'What can inpair fitness to drive?',
+        answer: [
+            { text: "Fatigue", correct: false },
+            { text: "Certain medicines", correct: true },
+            { text: "Alcohol and other intoxicants", correct: false },
+        ]
+    },
+    {
+        question: 'What should you do if you start feeling tired while driving?',
+        answer: [
+            { text: "Take a break straightaway", correct: true },
+            { text: "Get out of the car", correct: false },
+            { text: "Listen to stimulating music", correct: false },
+        ]
+    },
+    {
+        question: 'What emotions can influence driving behaviour?',
+        answer: [
+            { text: "Sorrow and worry", correct: false },
+            { text: "Happiness and exuberance", correct: false },
+            { text: "Anger and rage", correct: true },
+        ]
+    },
+    {
+        question: 'What can be the effect of even small quantities of alcohol?',
+        answer: [
+            { text: "Reckless driving", correct: true },
+            { text: "Delayed reactions", correct: false },
+            { text: "Impairment of hearing and vision", correct: false },
+        ]
+    },
+    {
+        question: 'Are drivers during their probation period allowed to be under the influence of alcohol when driving?',
+        answer: [
+            { text: "yes, up to 30 miligrams", correct: false },
+            { text: "No, definitely not", correct: true },
+            { text: "yes, upto 50 miligram", correct: false },
+        ]
+    },
+    {
+        question: 'When will offences carrying two points be deleted from the Central Register of Road Traffic Offenders?',
+        answer: [
+            { text: "-5 years", correct: true },
+            { text: "-2 years", correct: false },
+            { text: "-3 years", correct: false },
+        ]
+    },
+    {
+        question: 'You want to carry a child in your car, when must you use a child seat for this purpose?',
+        answer: [
+            { text: "If the child is older than 12", correct: false },
+            { text: "If the child is taller than 150 cm", correct: false },
+            { text: "If the child is younger than 12", correct: true },
+        ]
+    },
+    {
+        question: 'What is the maximum length of time you are allowed to stop at a bus stop provided you do not present an obstruction to buses?',
+        answer: [
+            { text: "3 minutes", correct: true },
+            { text: "8 minutes", correct: false },
+            { text: "5 minutes", correct: false },
+        ]
+    },
+    {
+        question: 'Is it irresponsible to overtake a truck and trailer a short distance before crossroads?',
+        answer: [
+            { text: "No, they drive at low speed", correct: false },
+            { text: "No, they understands", correct: false },
+            { text: "Yes, they can obscure traffic signs", correct: true },
+        ]
+    }
+];
